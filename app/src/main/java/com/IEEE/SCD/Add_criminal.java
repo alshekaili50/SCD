5a63d095f7d7d5f4aac69db7868ac59c746a9d7b
uae25 <alshekaili50@gmail.com>
2017-02-15 22:19:58 +0400

    hi

--- app/src/main/java/com/IEEE/SCD/Add_criminal.java
+++ app/src/main/java/com/IEEE/SCD/Add_criminal.java
@@ -29,6 +29,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Calendar;
 // Test github
+//HI
 public class Add_criminal extends AppCompatActivity {
 
     private int mYear;
